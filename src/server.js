7170bbdb896a672b805cbac39e6aa9301e6d6f84
Banzak-Uninter <leosantanabr@gmail.com>
2021-03-26 16:23:36 -0300

    reorganizing pastes

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express")
 const server = express()
 
+server.use(express.static("public"))
+
 server.get('/', (request, response) => {
     console.log('entrei no index')
 
-    return  response.send('deu certo')
+    return  response.sendFile(__dirname + '/views/index.html')
 })
 
 server.listen(3000, () =>  console.log('rodando'))
