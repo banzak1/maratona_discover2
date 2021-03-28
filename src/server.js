4f2340ca3b79811e3b8a9b9189593d5111e6fc17
banzak1 <leosantanabr@gmail.com>
2021-03-28 12:44:29 -0300

    configuring routes

--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 const express = require("express")
 const server = express()
+const routes = require("./routes")
 
+//habilitar arquivos statics
 server.use(express.static("public"))
 
-server.get('/', (request, response) => {
-    console.log('entrei no index')
-
-    return  response.sendFile(__dirname + '/views/index.html')
-})
+//routes
+server.use(routes)
 
 server.listen(3000, () =>  console.log('rodando'))
 
