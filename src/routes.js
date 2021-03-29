a0cf1e594bd602c47271fc54710e924de90f54c6
Banzak-Uninter <leosantanabr@gmail.com>
2021-03-29 16:51:17 -0300

    create profile objet

--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,21 @@ const routes = express.Router()
 // caminho views base
 const views = __dirname + "/views/"
 
+const profile = {
+    name: "Banzak",
+    avatar: "https://avatars.githubusercontent.com/u/17907755?v=4",
+    "monthly-budget": 3000,
+    "days-per-week": 5,
+    "hours-per-day": 5,
+    "vacation-per-year": 4
+}
+
 // request, response
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) =>  res.render(views + "job"))
 routes.get('/job-edit', (req, res) =>  res.render(views + "job-edit"))
-routes.get('/profile', (req, res) =>  res.render(views + "profile"))
+routes.get('/profile', (req, res) =>  res.render(views + "profile", { profile }))
+
 
 
 module.exports = routes;
