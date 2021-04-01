a75c2b0de4ba6c689fd164fbbf89d7ac24c40a4a
Banzak-Uninter <leosantanabr@gmail.com>
2021-04-01 15:41:22 -0300

    create new date to jobs

--- src/routes.js
+++ src/routes.js
@@ -16,9 +16,20 @@ const profile = {
     "vacation-per-year": 4
 }
 
+
+const jobs = []
+
 // request, response
 routes.get('/', (req, res) => res.render(views + "index"))
 routes.get('/job', (req, res) =>  res.render(views + "job"))
+routes.post('/job', (req, res) => {
+
+    const job = req.body
+    job.createdAt = new Date.now()//atribuindo uma nova data
+
+    jobs.push(req.body)
+    return res.redirect('/')
+})
 routes.get('/job-edit', (req, res) =>  res.render(views + "job-edit"))
 routes.get('/profile', (req, res) =>  res.render(views + "profile", { profile }))
 
